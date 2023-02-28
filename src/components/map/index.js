d5b2d2619035ac5274e60cab2e181aa440a14c71
Vulquimar-Silva <69085318+Vulquimar-Silva@users.noreply.github.com>
2023-02-28 01:57:35 -0300

    fix error component

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -5,20 +5,11 @@ import Marker from "../marker";
 const keyMap = `${process.env.KEY_MAP_GOOGLE}`
 
 const Map = () => {
-  // const { isLoaded } = useJsApiLoader({
-  //   id: 'google-map-script',
-  //   googleMapsApiKey: "AIzaSyDbR0-KMHG7b6aV2mxDIlTwCZwZewfZBSw"
-  // })
-
-  // const center = {
-  //   lat: -23.561684,
-  //   lng: -46.625378
-  // };
 
   return (
     <div className="container-map">
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyDbR0-KMHG7b6aV2mxDIlTwCZwZewfZBSw" }}
+        bootstrapURLKeys={{ key: keyMap }}
         center={{
           lat: -23.561684,
           lng: -46.625378,
